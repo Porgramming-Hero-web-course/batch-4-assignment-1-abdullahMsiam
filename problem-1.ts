53f920b57e99a9170edca1d9b15d50bb8e4bd096
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2024-12-13 16:54:23 +0600

    problem 1 submitted

--- problem-1.ts
+++ problem-1.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+{
+  const sumArray = (arr: number[]) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
+    return sum;
+  };
+  sumArray([1, 2, 3, 4, 5]);
+}
